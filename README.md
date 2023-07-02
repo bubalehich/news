e5d113d853a80ad994191f75fdeb6d76d1b07979
Bubalehich <janeridebmx@gmail.com>
2023-07-02 22:22:56 +0300

    fix: delete redundant code

--- README.md
+++ README.md
@@ -10,8 +10,3 @@
  ## Fake data
  Can be loaded with DataFixtures.
  `data-fixtures.enabled : true` in configurations.
- 
- ---
- 1. [Data fixtures](https://github.com/piotrpolak/spring-boot-data-fixtures) 0.2.0
- 2. [Data faker](https://github.com/DiUS/java-faker) 1.0.2
-3. 
--- news-microservice/src/main/java/ru/clevertec/news/NewsApplication.java
+++ news-microservice/src/main/java/ru/clevertec/news/NewsApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @OpenAPIDefinition(servers = @Server(url = "http://localhost:8080"))
 @SpringBootApplication
+@EnableFeignClients
 public class NewsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
